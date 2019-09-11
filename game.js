81264cfbc200c68d380ae40f02b25d41798623b5
Kodchaporn Satapanasatien <60070001@kmitl.ac.th>
2019-09-11 14:11:13 +0700

    -

--- game.js
+++ game.js
@@ -23,6 +23,17 @@ var binInt = parseInt(decResult);
 // calaulate bin to dec
 document.getElementById("demo5").innerHTML = bin2dec(binInt);
 
+//random button
+var btn = ["btn1", "btn2", "btn3", "btn4"];
+var correctAns = btn[rand(4)];
+
+
+//answer button
+document.getElementById(correctAns).innerHTML = bin2dec(binInt);
+// document.getElementById("btn2").innerHTML = rand(16);
+// document.getElementById("btn3").innerHTML = rand(16);
+// document.getElementById("btn4").innerHTML = rand(16);
+
 function bin2dec(bin){
 	return parseInt(bin, 2).toString(10);
 }
